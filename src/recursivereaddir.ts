40b55659895f1ed696e23b540d5eb3420c85ec5a
Magnus Kreth <magnus.kreth@gmx.de>
2021-05-17 19:00:11 +0200

    chore: add license header

--- src/recursivereaddir.ts
+++ src/recursivereaddir.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, Magnus Kreth.
+ * All rights reserved.
+ * Licensed under the MIT license.
+ * For full license text, see file LICENSE.txt in the repository root.
+ */
+
 import * as fs from 'fs';
 import recursiveReaddir = require('recursive-readdir');
 type IgnoreFunction = (file: string, stats: fs.Stats) => boolean;
